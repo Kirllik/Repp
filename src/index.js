528c1a80037a9f947592f06c6131831af647142a
Kirllik <krejg007@rambler.ru>
2022-01-15 23:39:56 +0300

    35 c коментами

--- src/index.js
+++ src/index.js
@@ -7,17 +7,18 @@ import App from './App';
 import {addPost, updateNewPostText} from './redux/state';
 import {BrowserRouter} from "react-router-dom";
 
+/*///////////////////////////////////////___ФУНКЦИЯ___///////////////////////////////////////////*/
 let rerenderEntireTree = (state) => {
     ReactDOM.render(
         <BrowserRouter>
             <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
         </BrowserRouter>, document.getElementById('root'));
 }
+/*///////////////////////////////////////////////////////////////////////////////*/
 
-rerenderEntireTree(state);
+rerenderEntireTree(state);  /*Вызов___ФУНКЦИ___*/
 
 subscribe(rerenderEntireTree);
-
 
 
 // If you want your app to work offline and load faster, you can change
